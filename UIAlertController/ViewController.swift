84a5e10a4b81b1c9c29254d1e299ecc37553b853
D7703_15 <d7703_15@D7703-15ui-iMac.local>
2018-04-19 11:09:42 +0900

    UIAlertController

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -12,14 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let myAlertController = UIAlertController(title: "알람", message: "설정된 시간이 되었습니다.", preferredStyle: .alert)
+        
+        //AlertAction 만들기
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: {(myAction:UIAlertAction) -> Void in self.view.backgroundColor = UIColor.blue
+        })
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: {(myAction:UIAlertAction) -> Void in self.view.backgroundColor = UIColor.white
+        })
+        
+        let testAction = UIAlertAction(title: "테스트", style: .default, handler: nil)
+        
+        //AlertAction을 AlertController에 넣기
+        myAlertController.addAction(okAction)
+        myAlertController.addAction(cancelAction)
+        myAlertController.addAction(testAction)
+        
+        //화면에 출력
+        present(myAlertController, animated: true, completion: nil)
     }
-
-
 }
-
